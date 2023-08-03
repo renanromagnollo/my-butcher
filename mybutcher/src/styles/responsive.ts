785052d8bc7074b87a427e29249004877d046ddc
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-08-03 08:18:18 -0300

    feat: breakpoints added

---
+++ mybutcher/src/styles/responsive.ts
@@ -0,0 +1,14 @@
+export const breakpoints = [
+    {
+        name: 'lg',
+        size: 1368,
+    },
+    {
+        name: 'md',
+        size: 1024,
+    },
+    {
+        name: 'sm',
+        media: 768
+    }
+]
